03c779008659e26d4a9f0907c65a770ce6090f41
Rohan Surti <surt0008@algonquilive.com>
2026-03-01 20:31:38 -0500

    Add Ubuntu Linux VM with SSH key and cloud-init

--- main.tf
+++ main.tf
@@ -122,3 +122,35 @@ data "cloudinit_config" "config" {
     content      = file("init.sh")
   }
 }
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-A05-VM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.region
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = data.cloudinit_config.config.rendered
+}
